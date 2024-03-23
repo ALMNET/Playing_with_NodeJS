1e4d8fc104df75245b1383c3a6d768bd02fa9255
Armando Almarales <armando.almarales87@mail.com>
2024-03-23 18:08:41 -0300

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,4 @@
+const saludos = require("./saludos.js");
+
+console.log(saludos.show_salute("Puto"));
+// console.log(salute.saluter("Puto")); // Tambien valido
---
+++ saludos.js
@@ -0,0 +1,12 @@
+function show_salute(msg)
+{
+    return `${msg} el que lee`;
+}
+
+function saludarHolaMundo()
+{
+    return `Hola Mundo`;
+}
+
+module.exports.show_salute = show_salute;
+// module.exports.saluter = show_salute; // Tambien valido
