ec139bf812ed0bc03ab400050d95f68313551f7b
Armando Almarales <armando.almarales87@mail.com>
2024-03-25 23:40:41 -0300

    Uso basico del modulo process, uso del argv

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-console.log("Hola");
+// console.log(process);
+// console.log(process.env);
 
-console.warn("Puede que tengas un error");
+// [node, app.js, 6, 7]
+//    0      1    2  3
 
-console.error(new Error("Tuviste un error"));
+// Ejemplo
+// console.log(process.argv[2]);
+// console.log(process.argv[3]);
+
+let i;
+
+for (i = 2; i < process.argv.length; i++)
+{
+    console.log(process.argv[i]);
+}
+
